f2f3ce4f74c086b1da9cd224907a4d1756306205
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-10-12 16:20:02 +0600

    tenth-commit

--- src/components/QuizDisplay/QuizDisplay.js
+++ src/components/QuizDisplay/QuizDisplay.js
@@ -18,3 +18,35 @@ const QuizDisplay = (param) => {
             toast.error("Wrong Answer")
         }
     }
+
+    let correctAnswer = () => {
+        toast(() => (
+            <div>
+                <span className='font-bold'>Correct Answer: </span> {param.data.correctAnswer}
+            </div>
+        ));
+    }
+    return (
+        <div>
+            <div><ToastContainer /></div>
+            <div className="card ">
+                <div className="card-body">
+                    <p className='card-question'>Question: {question.slice(3, -4)}</p>
+                    <div className="card-options">
+                        <button onClick={(e) => checker(e.target.value)}  value={opt[0]} >{opt[0]}</button>
+                        <button onClick={(e) => checker(e.target.value)}  value={opt[1]} >{opt[1]}</button>
+                        <button onClick={(e) => checker(e.target.value)}  value={opt[2]} >{opt[2]}</button>
+                        <button onClick={(e) => checker(e.target.value)}  value={opt[3]} >{opt[3]}</button>
+                        <br></br>
+                        <button onClick={() => correctAnswer()} className="eye-btn">
+                            <img src="https://cdn-icons-png.flaticon.com/512/159/159078.png" alt="" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default QuizDisplay;
