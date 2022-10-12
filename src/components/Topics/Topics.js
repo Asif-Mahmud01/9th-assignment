1eecc54897c9994923220f33bc686b4deb7b4452
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-10-12 16:16:03 +0600

    fifth-commit

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -8,3 +8,20 @@ const Topics = () => {
     const quizes = useLoaderData()
     console.log(quizes)
     return (
+        <div>
+            <div className='quiz-logo'>
+            <img src={quixLogo} alt="" />
+            </div>
+            <div  className='topics'>
+            {
+                quizes?.data?.map(quiz => <Quiz
+                key={quiz.id}
+                quiz={quiz}
+                ></Quiz>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Topics;
