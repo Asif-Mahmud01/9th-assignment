6fc3556f1c3cc92feda8c98bc2c18b3bdbfdc9de
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-10-12 16:14:15 +0600

    third-commit

--- src/App.js
+++ src/App.js
@@ -8,3 +8,51 @@ import Blog from './components/Blog/Blog';
 import QuizDetails from './components/QuizDetails/QuizDetails';
 import productsAndCartLoader from './components/UTools/UTools';
 
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          // loader: () => fetch('products.json'),
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Topics></Topics>
+        },
+        {
+          path: '/Topic',
+          // loader: () => fetch('products.json'),
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Topics></Topics>
+        },
+        {
+          path: '/Statistics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
+          // loader: productsAndCartLoader,
+          element: <Statistics></Statistics>
+        },
+        
+        {
+          path: 'Blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: '/quizdetails/:id',
+          loader: async ({ params }) => {
+            const { id } = params;
+            return fetch(`https://openapi.programming-hero.com/api/quiz/${id}`)
+          },
+          element: <QuizDetails></QuizDetails>
+        },
+      ]
+    }
+  ])
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
