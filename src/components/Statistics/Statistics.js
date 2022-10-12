36b779b6150bff15264e243f46ca46ee64d4125a
Asif-Mahmud01 <asifmahmud3.am@gmail.com>
2022-10-12 16:16:58 +0600

    sixth-commit

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import './Statistics.css'
+
